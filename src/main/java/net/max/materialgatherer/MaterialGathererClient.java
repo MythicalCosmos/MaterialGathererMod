b964143734902bbd6e2829bd65e44a99c378b601
MythicalCosmos <360mcv@gmail.com>
2023-12-21 14:25:58 -0700

    Base Project

---
+++ src/main/java/net/max/materialgatherer/MaterialGathererClient.java
@@ -0,0 +1,10 @@
+package net.max.materialgatherer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaterialGathererClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/max/materialgatherer/MaterialGathererDataGenerator.java
@@ -0,0 +1,11 @@
+package net.max.materialgatherer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MaterialGathererDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
